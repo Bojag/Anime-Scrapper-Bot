1845606dbcf89efefc9c9825b3950da82338503a
D9292S <kdeepankar105@gmail.com>
2021-03-19 18:31:59 +0530

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,9 +1,10 @@
 import discord
 from discord.ext import commands
+import os
 
 client = commands.Bot(command_prefix = "$")
 
-client.event
+@client.event
 async def on_ready():
     print("Bot is Ready....")
     await client.change_presence(status=discord.Status.online, activity=discord.Game(f"Watching my Owner...."))
