f06d6470c5dd1fc3a93b087e955e4e0b81521503
D9292S <kdeepankar105@gmail.com>
2021-04-21 18:21:36 +0530

    launcher file updated

--- launcher.py
+++ launcher.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import os
 
 client = commands.Bot(command_prefix = "$")
+TOKEN = "Add Your Bot Token here"
 
 @client.event
 async def on_ready():
@@ -21,5 +22,5 @@ for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         client.load_extension(f"cogs.{filename[:-3]}")
 
-client.run("TOKEN")
+client.run(TOKEN)
     
